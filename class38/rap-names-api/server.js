a5a14de1acfcc4f857d173fda166bfd933326a34
cerpinconsafo <pskolsky74@gmail.com>
2022-05-31 21:46:57 -0400

    massive commit with a few classes

---
+++ class38/rap-names-api/server.js
@@ -0,0 +1,42 @@
+const e = require('express')
+const express = require('express')
+const app = express()
+const PORT = 8000
+
+
+const rappers = {
+    '21 savage':{
+        'age': 29,
+        'birthName': 'Sheyaa Bin Abraham-Joseph',
+        'birthLocation': 'London, England',
+        },
+    'chance the rapper' :{
+        'age': 28,
+        'birthName': 'Chancellor Bennet',
+        'birthLocation': 'Chicago, Illinois',
+        },
+    'dylan' :{
+            'age': 29,
+            'birthName': 'Dylan',
+            'birthLocation': 'Dylan',
+            },
+    }
+
+app.get('/', (request, response) => {
+    response.sendFile(__dirname + '/index.html')
+})
+
+app.get('/api/:rapperName', (request, response) => {
+    const rappersName = request.params.rapperName.toLowerCase()
+   
+    if(rappers[rappersName]){
+            response.json(rappers[rappersName])
+    }
+    else {
+        response.json(rappers['dylan'])
+    }
+})
+
+app.listen(PORT, () => {
+    console.log(`The server be runnin on ${PORT}, ya dig?`)
+})
